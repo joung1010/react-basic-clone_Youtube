f38605494984d1a2587474f2a7fddef89c46ec53
mason park <joung4342@gmail.com>
2022-05-18 20:22:12 +0900

    css 수정 커밋

--- src/component/header/youtubeHeader.jsx
+++ src/component/header/youtubeHeader.jsx
@@ -1,19 +1,23 @@
 import React, {Component} from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faYoutubeSquare } from '@fortawesome/free-brands-svg-icons';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faYoutubeSquare} from '@fortawesome/free-brands-svg-icons';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
 import styles from './header.module.css'
 
 class YoutubeHeader extends Component {
     render() {
         return (
             <header className={styles.header}>
-                <span className={styles.icon}><FontAwesomeIcon icon={faYoutubeSquare} /></span>
-                <span className={styles.headerName}>Youtube</span>
-                <input className={styles.headerInput} type="text" placeholder="Search...."/>
-                <button className={styles.headerButton}>
-                    <FontAwesomeIcon icon={faSearch}/>
-                </button>
+                <div className={styles.headerNameBox}>
+                    <div className={styles.icon}><FontAwesomeIcon icon={faYoutubeSquare}/></div>
+                    <div className={styles.headerName}>Youtube</div>
+                </div>
+                <div className={styles.headerBox}>
+                    <input className={styles.headerInput} type="text" placeholder="Search...."/>
+                    <button className={styles.headerButton}>
+                        <FontAwesomeIcon icon={faSearch}/>
+                    </button>
+                </div>
             </header>
         );
     }
