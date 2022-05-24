48b18492ab2939949d30543fdf3fb85ccd7bf511
mason park <joung4342@gmail.com>
2022-05-24 20:09:31 +0900

    설명 추가

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -24,5 +24,12 @@ class Youtube {
     }
 
 }
+/*
+*   컴포넌트는 최대한 멍청하게 만들자 (무겁게 만들지 말자)
+*   외부 통신이 필요하면 그 부분만 새로 서비스로 만들어서 주입하자
+*   최상위 경로에 .env 환경 변수 파일을 만들고 여기에 필요한 값을 만들자
+*   이떄 prefix 로 REACT_APP 붙이고 사요은 process.env 로 접근가능
+*
+* */
 
 export default Youtube;
