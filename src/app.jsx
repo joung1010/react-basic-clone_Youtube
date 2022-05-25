2e08d6ad0bed471726de55dd2c2d0ea13419aa95
mason park <joung4342@gmail.com>
2022-05-25 22:30:19 +0900

    초기화

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,7 @@
-import React, {useEffect, useState} from 'react';
-import VideoList from "./component/video_list/video_list";
-import {logDOM} from "@testing-library/react";
+import React from 'react';
 
-const App = (props) => {
-    const [videoItems,setVideoItems] = useState([]);
-
-    useEffect(()=>{
-        const requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-        };
-
-        fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAbqjAMe0t9nrmoK55BlzHHLMlTmE6ASSA", requestOptions)
-            .then(response => response.json())
-            .then(result => setVideoItems(result.items))
-            .catch(error => console.log('error', error));
-    },[]);
-
-    return(
-       <VideoList videoItems={videoItems}/>
-)
-};
+const App = (props) => (
+    <h1>hi</h1>
+);
 
 export default App;
