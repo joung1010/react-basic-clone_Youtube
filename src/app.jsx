3ef54f38acce811df5449d20e3467f7e32ee4efe
mason park <joung4342@gmail.com>
2022-05-29 12:43:59 +0900

    search Item

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,21 @@ import YoutubeHeader from "./component/youtube_header/youtube_header";
 
 const App = (props) => {
     const [videoList,setVideoList] = useState([]);
+    const [searchItem, setSearchItem] = useState(null);
+
+    const handleSearch = (query) => {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch(`https://youtube.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=25&q=${query}&key=AIzaSyAbqjAMe0t9nrmoK55BlzHHLMlTmE6ASSA`,
+            requestOptions)
+            .then(response => response.json())
+            .then(result => result.items.map(item=>({...item , id:item.id.videoId})))
+            .then(items => setSearchItem(items))
+            .catch(error => console.log('error', error));
+    };
 
     useEffect(()=>{
         const requestOptions = {
@@ -17,9 +32,10 @@ const App = (props) => {
             .catch(error => console.log('error', error));
     },[]);
 
+
     return (
         <section>
-            <YoutubeHeader/>
+            <YoutubeHeader onSearch={handleSearch}/>
             <VideoList videoList={videoList}/>
         </section>
 
--- src/component/youtube_header/youtube_header.jsx
+++ src/component/youtube_header/youtube_header.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styles from './youtube_header.module.css'
 
-const YoutubeHeader = (props) => (
-   <header className={styles.header}>
-       <div className={styles.logo}>
-           <img className={styles.img} src="/img/logo.png" alt="logo"/>
-           <h2 className={styles.title}>YouTube</h2>
-       </div>
-      <input className={styles.input} type="search" placeholder={`...Search`}/>
-       <button className={styles.searchBtn}>
-           <img className={styles.btnImg} src="/img/search.png" alt="search"/>
-       </button>
-   </header>
-);
+const YoutubeHeader = ({onSearch}) => {
+    const inputRef = useRef();
+
+    const onHandleSearch = () => {
+        const query = inputRef.current.value;
+        onSearch(query);
+        inputRef.current.value ='';
+    };
+
+    const onHandleClick = () => {
+        onHandleSearch();
+    };
+
+    const onHandlePress = (event) => {
+        if (event.key === 'Enter') {
+            onHandleSearch();
+        }
+    };
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <img className={styles.img} src="/img/logo.png" alt="logo"/>
+                <h2 className={styles.title}>YouTube</h2>
+            </div>
+            <input ref={inputRef} className={styles.input} type="search" placeholder={`...Search`} onKeyPress={onHandlePress}/>
+            <button className={styles.searchBtn}>
+                <img className={styles.btnImg} src="/img/search.png" alt="search" onClick={onHandleClick}/>
+            </button>
+        </header>
+    );
+
+}
 
 export default YoutubeHeader;
