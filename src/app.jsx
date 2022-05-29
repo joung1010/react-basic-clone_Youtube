7acf65ff807a43bf838b3ffa6b46000799ef2671
mason park <joung4342@gmail.com>
2022-05-29 13:35:43 +0900

    search advanced

--- src/app.jsx
+++ src/app.jsx
@@ -2,34 +2,19 @@ import React, {useEffect, useState} from 'react';
 import VideoList from "./component/video_list/video_list";
 import YoutubeHeader from "./component/youtube_header/youtube_header";
 
-const App = (props) => {
+const App = ({youtube}) => {
     const [videoList,setVideoList] = useState([]);
-    const [searchItem, setSearchItem] = useState(null);
+    const [selectedVideo, setSelectedVideo] = useState(null);
 
     const handleSearch = (query) => {
-        const requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-        };
-
-        fetch(`https://youtube.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=25&q=${query}&key=AIzaSyAbqjAMe0t9nrmoK55BlzHHLMlTmE6ASSA`,
-            requestOptions)
-            .then(response => response.json())
-            .then(result => result.items.map(item=>({...item , id:item.id.videoId})))
-            .then(items => setSearchItem(items))
-            .catch(error => console.log('error', error));
+        youtube.search(query)
+            .then(items => setVideoList(items));
     };
 
     useEffect(()=>{
-        const requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-        };
+        youtube.mostPopular()
+            .then(items => setVideoList(items));
 
-        fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAbqjAMe0t9nrmoK55BlzHHLMlTmE6ASSA", requestOptions)
-            .then(response => response.json())
-            .then(result => setVideoList(result.items))
-            .catch(error => console.log('error', error));
     },[]);
 
 
