4c13a92e34cde58b66929b0cabcdbaf88851791c
mason park <joung4342@gmail.com>
2022-05-29 15:04:16 +0900

    about details

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import VideoList from "./component/video_list/video_list";
 import YoutubeHeader from "./component/youtube_header/youtube_header";
+import VideoDetail from "./component/video_detail/video_detail";
+import styles from './app.module.css'
 
 const App = ({youtube}) => {
     const [videoList,setVideoList] = useState([]);
@@ -11,6 +13,10 @@ const App = ({youtube}) => {
             .then(items => setVideoList(items));
     };
 
+    const handleClick = (video) => {
+        setSelectedVideo(video);
+    };
+
     useEffect(()=>{
         youtube.mostPopular()
             .then(items => setVideoList(items));
@@ -19,10 +25,20 @@ const App = ({youtube}) => {
 
 
     return (
-        <section>
+        <div className={styles.app}>
             <YoutubeHeader onSearch={handleSearch}/>
-            <VideoList videoList={videoList}/>
-        </section>
+            <section className={styles.content}>
+                {  selectedVideo &&
+                    <div className={styles.detail}>
+                        <VideoDetail video={selectedVideo}/>
+                    </div>
+                }
+                <div className={styles.list}>
+                    <VideoList videoList={videoList} onClickVideo={handleClick}
+                    display={selectedVideo ? "list" : "grid"}/>
+                </div>
+            </section>
+        </div>
 
     );
 };
--- src/component/video_list/video_list.jsx
+++ src/component/video_list/video_list.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import VideoItem from "../video_tiem/video_item";
 import styles from './video_list.module.css'
 
-const VideoList = ({videoList}) => (
+const VideoList = ({videoList,onClickVideo,display}) => (
     <ul className={styles.list}>
         {
             videoList.map(video => (
               <VideoItem
                   key={video.id}
                   video={video}
-
+                  onClickVideo={onClickVideo}
+                  display={display}
               />
             ))
         }
